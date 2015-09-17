5d84c7ed5d0c593adc9e385e2d3ab0fa6ab44ee7
Konstantin Sivakov <tino097@gmail.com>
2015-09-18 00:15:44 +0200

    port,ip

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +15,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
+http.listen(port,ip_address, function(){
   console.log('listening on *:8080');
 });
