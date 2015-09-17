f1f94d1882aa193421cd443d6f24046353b4e0b0
Konstantin Sivakov <tino097@gmail.com>
2015-09-18 00:48:22 +0200

    username test

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ var io = require('socket.io')(http);
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
+//var socket = io.listen(port, ip_address);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+  socket.on('chat message', function(msg, username){
+    io.emit('chat message', msg, username);
   });
 });
 
